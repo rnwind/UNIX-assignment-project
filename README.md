3420047bdcc52e46621b3afdb0cbbfe758fd9e50
rnwind <aprokopenko84@gmail.com>
2020-03-25 17:02:45 +0300

    fixed enter check

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Guessinggame Project v.0.1
-### Creation date: 03/25/20 15:56:40
-### Number of lines of code: 22
+### Creation date: 03/25/20 17:01:14
+### Number of lines of code: 28
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,28 @@
-function guessinggame {
-
-	echo "How many files are in the current directory ?"
-	read g
-	nf=$(ls -la | egrep "^[-]" | wc -l)
-
-	while [[ ! $g -eq $nf ]]
+function get_number {
+	local g="x"
+	local regexp="^[0-9]+$"
+	while [[ ! $g =~ $regexp ]]
 	do
-		q=$(($nf-$g))
-		if [[ q -gt 0 ]]
-		then
-			echo "Your guess is too low, try again..."
-		else
-			echo "Your guess is to high, try again..."
-		fi
 		read g
 	done
-
-	echo "Bingo! Thanks for playing!"
+	echo $g
 }
 
-guessinggame
+echo "How many files are in the current directory ? (Please enter a number >=0)"
+g=$(get_number)
+nf=$(ls -la | egrep "^[-]" | wc -l)
+
+while [[ ! $g -eq $nf ]]
+do
+	q=$(($nf-$g))
+	if [[ $q -gt 0 ]]
+	then
+		echo "Your guess is too low, try again..."
+	else
+		echo "Your guess is to high, try again..."
+	fi
+	g=$(get_number)
+done
+
+echo "Bingo! Thanks for playing!"
+
