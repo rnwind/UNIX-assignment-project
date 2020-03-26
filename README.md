3934c74776d308000c662e075625a9ec8efd5b8c
rnwind <aprokopenko84@gmail.com>
2020-03-26 19:45:43 +0300

    fix big numbers input

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Guessinggame Project v.0.1
-### Creation date: 03/26/20 18:52:08
+### Creation date: 03/26/20 19:44:42
 ### Number of lines of code: 28
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,7 @@
 function get_number {
 	local g="x"
 	local regexp="^([1-9]+[0-9]*)*0*$"
-	while [[ ! $g =~ $regexp ]] || [[ $g = "" ]]
+	while [[ ! $g =~ $regexp ]] || [[ $g = "" ]] || [[ $g -lt 0 ]]
 	do
 		read g
 	done
